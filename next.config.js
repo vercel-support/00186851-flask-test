d8e99ccf77e3279ab0581f687f617dc3f61d1c05
010122 <dev@margins.org>
2024-01-05 09:52:44 -0800

    Update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites()
+  {
+      return [
+          {
+              source: '/api/python/:path*',
+              destination:
+              process.env.NODE_ENV === 'development'
+                  ? 'http://127.0.0.1:8000/api/python/:path*'
+                  : '/api/python/',
+          }
+      ]
+  },
 }
 
 module.exports = nextConfig
